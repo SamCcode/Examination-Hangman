3bddcec2dfc9cf6156fe9b97a8e6acc6467da82c
beratukiqi <104987478+beratukiqi@users.noreply.github.com>
2023-01-23 21:34:39 +0100

    Input check functions

--- JavaScript/JS.js
+++ JavaScript/JS.js
@@ -64,17 +64,19 @@ let alphabet = [
   'Ö',
 ];
 
+let numOfGuesses = 0;
+
 // Array för gissade bokstäver
 let guessedChars = [];
 
 // Array för kvarstående bokstäver
 let availableChars = [];
 
-// Array för korrekt ord
-let correctWord = '';
-
 // Array för korrekta bokstäver
 let correctChars = [];
+
+// Array för korrekt ord
+let correctWord = '';
 
 // Array för antal försök/kroppsdelar
 let hangman = [];
@@ -85,19 +87,61 @@ let input = '';
 document.addEventListener('keydown', (event) => {
   if (window.event) {
     input = event.key.toUpperCase();
+    numOfGuesses++;
+    console.log('Num of guesses ', numOfGuesses);
   }
+
+  compareInput(input, correctChars);
+  console.log(guessedChars);
+
   console.log(input);
 });
 
 //funktion för att slumpa fram ett ord från countries listan och splitta ordet till en array
 
 function randomizeWord(listOfWords) {
-  let randomWord = Math.floor(Math.random() * countries.length);
-  correctWord = countries[randomWord];
+  let index = Math.floor(Math.random() * countries.length);
+  correctWord = countries[index];
+
+  console.log(correctWord);
   for (let i = 0; i < correctWord.length; i++) {
     correctChars.push(correctWord.charAt(i));
-    // console.log(correctWord.charAt(i));
+    console.log(correctWord.charAt(i));
   }
+  console.log(correctChars);
 }
 
 randomizeWord();
+
+function compareInput(inputValue) {
+  // Kolla först så de skriver en bokstav
+  let hasMatched = false;
+  for (let i = 0; i < alphabet.length; i++) {
+    if (inputValue === alphabet[i]) {
+      hasMatched = true;
+      break;
+    }
+  }
+  if (!hasMatched) {
+    alert('Please enter a letter from A - Ö!');
+  }
+
+  if (hasMatched) {
+    checkGuessedChars(inputValue, guessedChars);
+  }
+}
+
+function checkGuessedChars(inputValue, listToCheck) {
+  let match = false;
+  for (let i = 0; i < listToCheck.length; i++) {
+    if (inputValue === listToCheck[i]) {
+      alert('Already used letter, try again!');
+      match = true;
+      break;
+    }
+  }
+
+  if (match === false) {
+    guessedChars.push(inputValue);
+  }
+}
