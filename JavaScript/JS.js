57dd73739731f6ab073b4e7b8c8690da266050d2
beratukiqi <104987478+beratukiqi@users.noreply.github.com>
2023-01-23 19:32:59 +0100

    Input fungerar - VERSALER

--- JavaScript/JS.js
+++ JavaScript/JS.js
@@ -1,10 +1,68 @@
 // Arrays vi behöver
 
 // Array för ord
-const countries = ["FINLAND","NEDERLÄNDERNA","BELGIEN","BULGARIEN","SPANIEN","IRLAND","ITALIEN","ÖSTERRIKE","GREKLAND","KROATIEN","CYPERN","LETTLAND","LITAUEN","LUXENBURG","MALTA","PORTUGAL","POLEN","FRANKRIKE","RUMÄNIEN","SVERIGE","TYSKLAND","SLOVAKIEN","SLOVENIEN","DANMARK","TJECKIEN","UNGERN","ESTLAND"];
+const countries = [
+  'FINLAND',
+  'NEDERLÄNDERNA',
+  'BELGIEN',
+  'BULGARIEN',
+  'SPANIEN',
+  'IRLAND',
+  'ITALIEN',
+  'ÖSTERRIKE',
+  'GREKLAND',
+  'KROATIEN',
+  'CYPERN',
+  'LETTLAND',
+  'LITAUEN',
+  'LUXENBURG',
+  'MALTA',
+  'PORTUGAL',
+  'POLEN',
+  'FRANKRIKE',
+  'RUMÄNIEN',
+  'SVERIGE',
+  'TYSKLAND',
+  'SLOVAKIEN',
+  'SLOVENIEN',
+  'DANMARK',
+  'TJECKIEN',
+  'UNGERN',
+  'ESTLAND',
+];
 
 // Array för bokstäver
-let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Å","Ä","Ö"];
+let alphabet = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'Å',
+  'Ä',
+  'Ö',
+];
 
 // Array för gissade bokstäver
 let guessedChars = [];
@@ -13,31 +71,33 @@ let guessedChars = [];
 let availableChars = [];
 
 // Array för korrekt ord
-let correctWord = "";
+let correctWord = '';
 
 // Array för korrekta bokstäver
 let correctChars = [];
 
-// Array för antal försök/kroppsdelar 
+// Array för antal försök/kroppsdelar
 let hangman = [];
 
+let input = '';
+
+// Tracka keypress hela tiden. Uppdatera senaste bokstav
+document.addEventListener('keydown', (event) => {
+  if (window.event) {
+    input = event.key.toUpperCase();
+  }
+  console.log(input);
+});
 
 //funktion för att slumpa fram ett ord från countries listan och splitta ordet till en array
 
-function randomizeWord (listOfWords) {
-    let randomWord = Math.floor(Math.random() * countries.length);
-    correctWord = countries[randomWord];
-    for (let i =0; i < correctWord.length; i++) {
-        correctChars.push(correctWord.charAt(i))
-        // console.log(correctWord.charAt(i));
-    }
-}
- randomizeWord()
- 
-function handleInput() {
-    document.addEventListener("keydown", () => {
-        console.log("hej");
-    }) 
+function randomizeWord(listOfWords) {
+  let randomWord = Math.floor(Math.random() * countries.length);
+  correctWord = countries[randomWord];
+  for (let i = 0; i < correctWord.length; i++) {
+    correctChars.push(correctWord.charAt(i));
+    // console.log(correctWord.charAt(i));
+  }
 }
 
-
+randomizeWord();
