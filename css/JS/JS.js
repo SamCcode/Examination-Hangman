d3217eec4bc31977b624d94c00aac6c7fcdfd400
SamCcode <115998847+SamCcode@users.noreply.github.com>
2023-01-23 17:44:36 +0100

    Lagt in alla listor

---
+++ css/JS/JS.js
@@ -0,0 +1,23 @@
+// Arrays vi behöver
+
+// Array för ord
+var countries = ["FINLAND","NEDERLÄNDERNA","BELGIEN","BULGARIEN","SPANIEN","IRLAND","ITALIEN","ÖSTERRIKE","GREKLAND","KROATIEN","CYPERN","LETTLAND","LITAUEN","LUXENBURG","MALTA","PORTUGAL","POLEN","FRANKRIKE","RUMÄNIEN","SVERIGE","TYSKLAND","SLOVAKIEN","SLOVENIEN","DANMARK","TJECKIEN","UNGERN","ESTLAND"];
+
+// Array för bokstäver
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Å","Ä","Ö"];
+
+// Array för gissade bokstäver
+var guessedChars = [];
+
+// Array för kvarstående bokstäver
+var availableChars = [];
+
+// Array för korrekt ord
+var correctWord = [];
+
+// Array för korrekta bokstäver
+var correctChars = [];
+
+// Array för antal försök/kroppsdelar 
+var hangman = [];
+ 
